91e2d91b35fced1efc0833562bc7dda4e29757dc
rahulkumar1711 <60355624+rahulkumar1711@users.noreply.github.com>
2020-07-03 21:47:11 +0530

    Added Flipping Player According to Input
    Added Code To Accept horizontal input from user
    Added Code To play run animation and flip player according to the horizontal input

--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -4,6 +4,23 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+	public Animator anim;
+	private void Update()
+	{
+		float speed = Input.GetAxisRaw("Horizontal");
+		anim.SetFloat("Speed", Mathf.Abs(speed));
+
+		Vector3 scale = transform.localScale;
+		if (speed < 0)
+		{
+			scale.x = -1f * Mathf.Abs(scale.x);
+		}
+		else if (speed > 0)
+		{
+			scale.x = Mathf.Abs(scale.x);
+		}
+		transform.localScale = scale;
+	}
 	private void OnCollisionEnter2D(Collision2D collision)
 	{
 		Debug.Log("Player on "+collision.gameObject.name);
