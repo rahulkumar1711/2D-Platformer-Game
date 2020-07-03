7ccca2712f3c43a39ecbedd9831931e633104a28
rahulkumar1711 <60355624+rahulkumar1711@users.noreply.github.com>
2020-07-03 20:26:01 +0530

    Added BoxCollider
    Done Collision Detection

--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		Debug.Log("Player on "+collision.gameObject.name);
+	}
 }
