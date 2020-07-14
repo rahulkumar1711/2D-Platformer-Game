37d9d3a98474f1fa28fcee0a0b8ced439629198b
Rahul Kumar <60355624+rahulkumar1711@users.noreply.github.com>
2020-07-14 21:06:31 +0530

    Added Lobby Scene with play button

---
+++ Scripts/GameOverController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameOverController : MonoBehaviour
+{
+	public Button buttonRestart;
+	private void Awake()
+	{
+		buttonRestart.onClick.AddListener(() => RelodLevel());
+	}
+
+	private void RelodLevel()
+	{
+		SceneManager.LoadScene(0);
+	}
+
+	public void PlayerDied()
+	{
+		gameObject.SetActive(true);
+	}
+}
